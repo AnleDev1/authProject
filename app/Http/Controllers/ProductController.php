bd671bb275ab46f1f7498fd74776741ba7846b16
Anderson <lm22001@itcha.edu.sv>
2025-06-01 16:08:18 -0600

    Se agrego el catalago de productos

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /*METODO QUE DEVUELVE TODOS LOS PRODUCTOS REGISTRADOS*/
+    public function getProducts() {
+        $products = Product::all();
+        if ($products->isEmpty()) {
+            return response()->json(['message' => 'No hay productos registrados'], 404);
+        }
+        return response()->json(['products' => $products], 200);
+    }
+
+    /*METODO QUE DEVUEL UN PRODUCTO MEDIANTE EL ID*/
+    public function getProductById($id){
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'El producto no existe'], 404);
+        }
+        return response()->json(['product' => $product], 200);
+    }
+
+    /*METODO QUE REGISTRA UN PRODUCTO NUEVO EN LA BASE DE DATOS*/
+    public function addProduct(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|min:2|max:100',
+            'price' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+        Product::create([
+            'name' => $request->get('name'),
+            'price' => $request->get('price')
+        ]);
+        return response()->json(['message' => 'Producto agregado correctamente'], 201);
+    }
+
+    /*ESTA FUNCION ACTUALIZA UN PRODUCTO YA REGISTRADO */
+    public function updateProductById(Request $request, $id) {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'El producto no existe'], 404);
+        }
+         $validator = Validator::make($request->all(),[
+            'name' => 'sometimes|string|min:2|max:100',
+            'price' => 'sometimes|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+        if ($request->has('name')) {
+            $product->name = $request->name;
+        }
+        if ($request->has('price')) {
+            $product->price = $request->price;
+        }
+        $product->update();
+        return response()->json(['message' => 'Producto Actualizado'], 200);
+    }
+
+    /*ELIMINA UN PRODUCTO*/
+    public function deleteProductById($id) {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'El producto no existe'], 404);
+        }
+        $product->delete();
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+
+    //NOTA: AGREGADO DESDE SITIO DE COFIG DE JWT https://jwt-auth.readthedocs.io/en/develop/quick-start/
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    //NOTA: AGREGADO DESDE SITIO DE COFIG DE JWT https://jwt-auth.readthedocs.io/en/develop/quick-start/
+     /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /* **************************** */
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +45,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'role',
         'email',
         'password',
     ];
